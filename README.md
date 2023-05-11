c2c3b0d95471a29f8fa6d92819eee557d8744e93
Cypphi <altcypphi@gmail.com>
2023-05-11 17:47:21 +0200

    It finally works

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Survival Debugger
-Use debug sticks in survival.
+Simple mod allowing you to use debug sticks in survival.
 
 ## Disclaimer
-This mod does not provide any ways of obtaining the debug stick in survival. This mod assumes you already have one through one of the numerous ways of obtaining one in 100% survival _(or by simply spawning it in)_.
+This mod contains code from the github repository listed below. This is a more simplified version which does not include a recipe to craft a debug stick in survival. This mod assumes you already have one through any of the countless ways of getting one in survival, creative or with commands.
+For ways to obtain a debug stick in survival check out this [amazing community](https://discord.gg/KXTaEftvj9).
+
+# Credits
+- JustS-js for [debugStickSurvival](https://github.com/JustS-js/debugStickSurvival)
--- src/main/java/cypphi/survivaldebugger/mixin/DebugStickItemMixin.java
+++ src/main/java/cypphi/survivaldebugger/mixin/DebugStickItemMixin.java
@@ -10,8 +10,10 @@ import net.minecraft.registry.Registries;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Property;
 import net.minecraft.text.Text;
+import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldAccess;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,43 +23,70 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Collection;
 
 @Mixin(DebugStickItem.class)
-public abstract class DebugStickItemMixin {
+public class DebugStickItemMixin {
     @Shadow
     private static void sendMessage(PlayerEntity player, Text message) {}
 
     @Shadow
-    private static <T extends Comparable<T>> BlockState cycle(BlockState state, Property<T> property, boolean inverse) { return null; }
+    private static <T extends Comparable<T>> String getValueString(BlockState state, Property<T> property) {return null;}
+
+    @Shadow
+    private static <T extends Comparable<T>> BlockState cycle(BlockState state, Property<T> property, boolean inverse) {
+        return null;
+    }
 
     @Inject(at = @At("HEAD"), method = "use", cancellable = true)
-    public void use(PlayerEntity player, BlockState state, WorldAccess world, BlockPos pos, boolean update, ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+    private void onUSE(PlayerEntity player, BlockState state, WorldAccess world, BlockPos pos, boolean update, ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        if (player.isCreativeLevelTwoOp()) {return;}
+
         Block block = state.getBlock();
         StateManager<Block, BlockState> stateManager = block.getStateManager();
         Collection<Property<?>> collection = stateManager.getProperties();
-        String string = Registries.BLOCK.getId(block).toString();
-        if (collection.isEmpty()) {
-            sendMessage(player, Text.of(String.format("Block: %s, No properties", string)));
-            cir.setReturnValue(false);
-        } else {
-            NbtCompound nbtCompound = stack.getOrCreateSubNbt("DebugProperty");
-            String string2 = nbtCompound.getString(string);
-            Property<?> property = stateManager.getProperty(string2);
-            if (update) {
-                if (property == null) {
-                    property = (Property)collection.iterator().next();
-                }
 
-                BlockState blockState = cycle(state, property, player.shouldCancelInteraction());
-                world.setBlockState(pos, blockState, 18);
-                sendMessage(player, Text.of(String.format("Block: %s, Property: %s, Value: %s", string, property.getName(), blockState.get(property))));
-            } else {
-//                I don't know how to do this - Github Copilot
-//                property = (Property)cycle((Iterable)collection, (Object)property, player.shouldCancelInteraction());
-                String string3 = property.getName();
-                nbtCompound.putString(string, string3);
-                sendMessage(player, Text.of(String.format("Block: %s, Property: %s", string, string3)));
+        if (!isBlockAllowedToModify(state.getBlock()) || collection.isEmpty()) {
+            sendMessage(player, Text.of("This block cannot be modified."));
+            cir.setReturnValue(false);
+            return;
+        }
+
+        NbtCompound nbtCompound = stack.getOrCreateSubNbt("DebugProperty");
+
+        String blockName = Registries.BLOCK.getId(block).toString();
+        String propertyName = nbtCompound.getString(blockName);
+
+        Property<?> property = stateManager.getProperty(propertyName);
+
+        if (player.isSneaking()) {
+            property = getNextProperty(collection, property, block);
+            nbtCompound.putString(blockName, property.getName());
+
+            sendMessage(player, Text.of(String.format("Property «%s» is now selected.", property.getName())));
+        } else {
+            if (property == null) {
+                property = getNextProperty(collection, null, block);
             }
 
-            cir.setReturnValue(true);
+            BlockState newState = cycle(state, property, false);
+            world.setBlockState(pos, newState, 18);
+            sendMessage(player, Text.of(String.format("Set property «%s» to «%s».", property.getName(), getValueString(newState, property))));
         }
+        cir.setReturnValue(true);
+    }
+
+    private Property<?> getNextProperty(Collection<Property<?>> collection, @Nullable Property<?> property, @Nullable Block block) {
+        int len = collection.size();
+        do {
+            property = Util.next(collection, property);
+            len--;
+        } while (len > 0 && !isPropertyModifiable(property, block));
+        return property;
+    }
+
+    private boolean isBlockAllowedToModify(Block block) {
+        return true;
+    }
+
+    private boolean isPropertyModifiable(Property<?> property, @Nullable Block block) {
+        return true;
     }
 }
